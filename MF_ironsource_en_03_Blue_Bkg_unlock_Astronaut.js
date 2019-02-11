41cef440d8380edc60141eb61fa57f7a6a4d0339
timapasechnik <timapasechnik@gmail.com>
2019-02-11 22:35:27 +0200

    Update MF_ironsource_en_03_Blue_Bkg_unlock_Astronaut.js

--- MF_ironsource_en_03_Blue_Bkg_unlock_Astronaut.js
+++ MF_ironsource_en_03_Blue_Bkg_unlock_Astronaut.js
@@ -17752,7 +17752,7 @@ function InitGameCanvas(lib, img, cjs, ss) {
           iBtnB = installButton.getBounds();
           var btnLoc = topRight.pointLoc0;
           installButton.x = - 1.5*iBtnB.width * iBtnScale;
-          installButton.y = 3; //bounds.height*0.5;
+          installButton.y = 5; //bounds.height*0.5;
 
           topRightBTN.addChild(installButton);
         } else {
